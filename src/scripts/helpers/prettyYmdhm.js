36eeaf04fb5d3c65067f8e0ba94a669d0abd2261
Denis Vybornyj <vybornyj@users.noreply.github.com>
2020-08-04 05:20:15 +0300

    re

--- src/scripts/helpers/prettyYmdhm.js
+++ src/scripts/helpers/prettyYmdhm.js
@@ -1,8 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prettyYmdhm = void 0;
-var with0 = function (num) { return "" + (num > 9 ? '' : 0) + num; };
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.prettyYmdhm = void 0
+var with0 = function (num) {
+  return '' + (num > 9 ? '' : 0) + num
+}
 exports.prettyYmdhm = function (date) {
-    var d = new Date(date);
-    return d.getFullYear() + "." + with0(d.getMonth() + 1) + "." + with0(d.getDate()) + " " + with0(d.getHours()) + ":" + with0(d.getMinutes());
-};
+  var d = new Date(date)
+  return d.getFullYear() + '.' + with0(d.getMonth() + 1) + '.' + with0(d.getDate()) + ' ' + with0(d.getHours()) + ':' + with0(d.getMinutes())
+}
